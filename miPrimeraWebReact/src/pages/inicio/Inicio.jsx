429f5cdd858cae1f0b54ac5a2002f8bc42b27245
Guillermovillacuratorres <127245210+Guillermovillacuratorres@users.noreply.github.com>
2026-09-16 15:06:24 -0300

    Clase 16-09-2026

--- miPrimeraWebReact/src/pages/inicio/Inicio.jsx
+++ miPrimeraWebReact/src/pages/inicio/Inicio.jsx
@@ -1,16 +1,43 @@
 import estilos from './inicio.module.css';
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 function Inicio() {
-    return(
-            <>
 
+
+
+
+
+    return(
+            
+            <>
+            
             <h1 className={estilos.titulo}>Hooola</h1>
             <img src="https://img.magnific.com/psd-gratis/linda-escena-perro-marron-blanco_23-2150179279.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
             <button>Guardar</button>
 
+            
             </>
+
+            
             
     );
 }
 
-export default Inicio;
+function BasicExample() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+
+export default BasicExample;
